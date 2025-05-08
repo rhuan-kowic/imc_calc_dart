dbb1b229e4a9ca1ebf144d7d92eac5e0494985ee
Rhuan <dev.rhuankowic@gmail.com>
2025-05-08 08:25:16 -0300

    Inicializando o projeto.

---
+++ bin/imc_calc_dart.dart
@@ -0,0 +1,5 @@
+import 'package:imc_calc_dart/imc_calc_dart.dart' as imc_calc_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${imc_calc_dart.calculate()}!');
+}
---
+++ test/imc_calc_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:imc_calc_dart/imc_calc_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
